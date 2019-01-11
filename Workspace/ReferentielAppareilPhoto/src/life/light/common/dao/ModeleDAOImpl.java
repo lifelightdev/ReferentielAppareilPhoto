84380991b3ea422a8d4443fc1ee15fcc8e4f7743
lifelightdev@gmail.com <lifelightdev@gmail.com>
2019-01-11 14:35:03 +0100

    Ajout des classes DAO

---
+++ Workspace/ReferentielAppareilPhoto/src/life/light/common/dao/ModeleDAOImpl.java
@@ -0,0 +1,81 @@
+package life.light.common.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import life.light.common.bean.Modele;
+
+import life.light.common.persistence.HibernateUtil;
+
+public class ModeleDAOImpl implements ModeleDAO<Modele, Integer> {
+
+	private Session currentSession;
+
+	private Transaction currentTransaction;	
+
+	public Session openCurrentSession() {
+		currentSession = HibernateUtil.getSessionFactory().openSession();
+		return currentSession;
+	}
+
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = HibernateUtil.getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+
+	public void closeCurrentSession() {
+		currentSession.close();
+	}
+
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}	
+
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void persist(Modele entity) {
+		getCurrentSession().save(entity);
+	}
+
+	public void update(Modele entity) {
+		getCurrentSession().update(entity);
+	}
+
+	public Modele findById(Integer id) {
+		Modele modele = (Modele) getCurrentSession().get(Modele.class, id);
+		return modele;
+	}
+
+	public void delete(Modele entity) {
+		getCurrentSession().delete(entity);
+	}
+
+	public List<Modele> findAll() {
+		List<Modele> modeles = (List<Modele>) getCurrentSession().createQuery("from Modele").list();
+		return modeles;
+	}
+
+	public void deleteAll() {
+		List<Modele> entityList = findAll();
+		for (Modele entity : entityList) {
+			delete(entity);
+		}
+	}
+}
---
+++ Workspace/ReferentielAppareilPhoto/src/life/light/common/dao/TypeDAOImpl.java
@@ -0,0 +1,84 @@
+package life.light.common.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import life.light.common.bean.Type;
+
+import life.light.common.persistence.HibernateUtil;
+
+public class TypeDAOImpl implements TypeDAO<Type, Integer> {
+
+	private Session currentSession;
+
+	private Transaction currentTransaction;	
+
+	public Session openCurrentSession() {
+		currentSession = HibernateUtil.getSessionFactory().openSession();
+		return currentSession;
+	}
+
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = HibernateUtil.getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+
+	public void closeCurrentSession() {
+		currentSession.close();
+	}
+
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}	
+
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void persist(Type entity) {
+		getCurrentSession().save(entity);
+	}
+
+	public void update(Type entity) {
+		getCurrentSession().update(entity);
+	}
+
+	public Type findById(Integer id) {
+		Type type = (Type) getCurrentSession().get(Type.class, id);
+		return type;
+	}
+
+	public void delete(Type entity) {
+		getCurrentSession().delete(entity);
+	}
+
+	public List<Type> findAll() {
+		List<Type> types = (List<Type>) getCurrentSession().createQuery("from Type").list();
+		return types;
+	}
+
+	public void deleteAll() {
+		List<Type> entityList = findAll();
+		for (Type entity : entityList) {
+			delete(entity);
+		}
+	}
+
+
+
+}
