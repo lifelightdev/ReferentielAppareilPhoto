09d9997cb8ce488a973894505ba4c5e08bb607c4
Life Light <lifelightdev@gmail.com>
2020-03-12 17:34:24 +0100

    Test avec mock du DAO

--- Workspace/ReferentielAppareilPhoto/src/main/java/life/light/referentielAppareilPhoto/web/controller/AppareilControllerTest.java
+++ Workspace/ReferentielAppareilPhoto/src/main/java/life/light/referentielAppareilPhoto/web/controller/AppareilControllerTest.java
@@ -1,41 +1,65 @@
 package life.light.referentielAppareilPhoto.web.controller;
 
+import life.light.referentielAppareilPhoto.dao.AppareilDao;
+import life.light.referentielAppareilPhoto.model.Appareil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(AppareilController.class)
 class AppareilControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private AppareilDao appareilDao;
+
     @Test
     void listeAppareil() throws Exception {
+        List<Appareil> listeAppareil = new ArrayList<>();
+        when(appareilDao.findAll()).thenReturn(listeAppareil);
         this.mockMvc.perform(get("/Appareil")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("[{\"id\":1,\"modeEmploie\":1,\"modele\":1},{\"id\":2,\"modeEmploie\":2,\"modele\":2},{\"id\":3,\"modeEmploie\":1,\"modele\":3}]")));
-    }
+                .andExpect(content().string(containsString("[]")));
+ }
 
     @Test
     void afficherUnAppareil() throws Exception {
-        this.mockMvc.perform(get("/AppareilByModele/1")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("[{\"id\":1,\"modeEmploie\":1,\"modele\":1}]")));
+        Appareil appareil = new Appareil(1,1,1);
+        when(appareilDao.findById(1)).thenReturn(appareil);
+        this.mockMvc.perform(get("/Appareil/1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("{\"id\":1,\"modeEmploie\":1,\"modele\":1}")));
     }
 
     @Test
     void listeAppareilParModele() throws Exception {
+        List<Appareil> listeAppareil = new ArrayList<>();
+        Appareil appareil = new Appareil(1,1,1);
+        listeAppareil.add(appareil);
+        when(appareilDao.findByModele(1)).thenReturn(listeAppareil);
         this.mockMvc.perform(get("/AppareilByModele/1")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("[{\"id\":1,\"modeEmploie\":1,\"modele\":1}]")));
+    }
+
+    @Autowired
+    private AppareilController controller;
+
+    @Test
+    public void contexLoads() {
+        assertThat(controller).isNotNull();
     }
 
 }
